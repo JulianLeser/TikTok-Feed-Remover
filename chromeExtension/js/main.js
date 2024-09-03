75e6e6c6a775c610acd3b4f317e5458f97f8df79
Julian Leser <leser.julian.noah@gmail.com>
2024-09-03 20:17:12 +0200

    Initial commit

---
+++ chromeExtension/js/main.js
@@ -0,0 +1,19 @@
+if (window.location.href === "https://www.tiktok.com/" || window.location.href === "https://www.tiktok.com") {
+    window.location.href = "https://www.tiktok.com/following/";
+} else if (window.location.href.includes("tiktok.com")) {
+    console.log("Running on TikTok:", window.location.href);
+
+    const intervalId = setInterval(() => {
+        console.log("Removed Feed");
+        const list = document.getElementsByClassName("css-nwhccz-UlMainNav")[0];
+        if (list && list.children.length >= 2) {
+            // Remove the first two list items
+            list.removeChild(list.children[0]);
+            list.removeChild(list.children[0]);
+
+            // Clear the interval once the task is done
+            clearInterval(intervalId);
+            console.log("List items removed");
+        }
+    }, 1000);  // Check every second
+}
