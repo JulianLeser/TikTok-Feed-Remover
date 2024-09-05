97cfe167da42112f403ebe471dc2e1a3b0a43542
Julian Leser <leser.julian.noah@gmail.com>
2024-09-05 15:06:44 +0200

    Added a redirect for the main page in all languages

--- chromeExtension/js/main.js
+++ chromeExtension/js/main.js
@@ -2,7 +2,9 @@ if (window.location.href === "https://www.tiktok.com/" || window.location.href =
     window.location.href = "https://www.tiktok.com/following/";
 } else if (window.location.href === "https://www.tiktok.com/explore" || window.location.href === "https://www.tiktok.com/explore") {
     window.location.href = "https://www.tiktok.com/following/";
-} else if (window.location.href.includes("tiktok.com")) {
+} else if (window.location.href.includes("tiktok.com/?lang=")) {
+    window.location.href = "https://www.tiktok.com/following/";
+}else if (window.location.href.includes("tiktok.com")) {
     console.log("Running on TikTok:", window.location.href);
 
     const intervalId = setInterval(() => {
